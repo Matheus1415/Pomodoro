96eb7451f1631cacec6d19d9d84e8af6887cf992
Matheus1415 <115950084+Matheus1415@users.noreply.github.com>
2024-02-24 09:16:20 -0300

    projeto concluido

--- script-crud.ts
+++ script-crud.ts
@@ -5,7 +5,8 @@ interface Tarefa {
 
 interface EstadoAplicacao {
     tarefas: Tarefa[]
-    tarefaSelecionada: Tarefa | null
+    tarefaSelecionada: Tarefa | null,
+    editando: boolean
 }
 
 let estadoInicial: EstadoAplicacao = {
@@ -23,10 +24,11 @@ let estadoInicial: EstadoAplicacao = {
             concluida: false
         }
     ],
-    tarefaSelecionada: null
+    tarefaSelecionada: null,
+    editando: false
 }
 
-const selecionarTarefa = (estado: EstadoAplicacao, tarefa: Tarefa) : EstadoAplicacao => {
+const selecionarTarefa = (estado: EstadoAplicacao, tarefa: Tarefa): EstadoAplicacao => {
 
     return {
         ...estado,
@@ -34,11 +36,37 @@ const selecionarTarefa = (estado: EstadoAplicacao, tarefa: Tarefa) : EstadoAplic
     }
 }
 
-const adicionarTarefa = (estado: EstadoAplicacao, tarefa: Tarefa) : EstadoAplicacao => {
+const adicionarTarefa = (estado: EstadoAplicacao, tarefa: Tarefa): EstadoAplicacao => {
     return {
         ...estado,
         tarefas: [...estado.tarefas, tarefa]
     }
+}
+
+// Deleta uma tarefa. Retorna um novo estado.
+const deletar = (estado: EstadoAplicacao): EstadoAplicacao => {
+    if (estado.tarefaSelecionada) {
+        const tarefas = estado.tarefas.filter(t => t != estado.tarefaSelecionada);
+        return { ...estado, tarefas, tarefaSelecionada: null, editando: false };
+    } else {
+        return estado;
+    }
+}
+
+// Deleta todas as tarefas. Retorna um novo estado.
+const deletarTodas = (estado: EstadoAplicacao): EstadoAplicacao => {
+    return { ...estado, tarefas: [], tarefaSelecionada: null, editando: false };
+}
+
+// Deleta todas as tarefas concluídas. Retorna um novo estado.
+const deletarTodasConcluidas = (estado: EstadoAplicacao): EstadoAplicacao => {
+    const tarefas = estado.tarefas.filter(t => !t.concluida);
+    return { ...estado, tarefas, tarefaSelecionada: null, editando: false };
+}
+
+// Modifica o estado para entrar no modo de edição. Retorna um novo estado.
+const editarTarefa = (estado: EstadoAplicacao, tarefa: Tarefa): EstadoAplicacao => {
+    return { ...estado, editando: !estado.editando, tarefaSelecionada: tarefa };
 }
 
 const atualizarUI = () => {
@@ -51,54 +79,63 @@ const atualizarUI = () => {
                 fill="#01080E" />
         </svg>
     `
-    const ulTarefas = document.querySelector('.app__section-task-list');
-    const formAdicionarTarefa = document.querySelector<HTMLFormElement>('.app__form-add-task');
-    const btnAdicionarTarefa = document.querySelector<HTMLButtonElement>('.app__button--add-task');
-    const textarea = document.querySelector<HTMLTextAreaElement>('.app__form-textarea');
-    const btnCancelar = document.querySelector<HTMLButtonElement>(".app__form-footer__button--cancel");
-    const btnDeletar = document.querySelector<HTMLButtonElement>(".app__form-footer__button--delete");
+    const ulTarefas = document.querySelector('.app__section-task-list')
+    const formAdicionarTarefa = document.querySelector<HTMLFormElement>('.app__form-add-task')
+    const btnAdicionarTarefa = document.querySelector<HTMLButtonElement>('.app__button--add-task')
+    const textarea = document.querySelector<HTMLTextAreaElement>('.app__form-textarea')
+    const labelTarefaAtiva = document.querySelector<HTMLParagraphElement>('.app__section-active-task-description')
+    const btnCancelar: HTMLButtonElement = document.querySelector('.app__form-footer__button--cancel') as HTMLButtonElement
+    const btnDeletar: HTMLButtonElement = document.querySelector('.app__form-footer__button--delete') as HTMLButtonElement
 
+    const btnDeletarConcluidas: HTMLButtonElement = document.querySelector('#btn-remover-concluidas') as HTMLButtonElement
+    const btnDeletarTodas: HTMLButtonElement = document.querySelector('#btn-remover-todas') as HTMLButtonElement
+
+    labelTarefaAtiva!.textContent = estadoInicial.tarefaSelecionada ? estadoInicial.tarefaSelecionada.descricao : null
+
+    if (estadoInicial.editando && estadoInicial.tarefaSelecionada) {
+        formAdicionarTarefa!.classList.remove('hidden')
+        textarea!.value = estadoInicial.tarefaSelecionada.descricao
+    } else {
+        formAdicionarTarefa!.classList.add('hidden')
+        textarea!.value = ''
+    }
 
     if (!btnAdicionarTarefa) {
         throw Error("Caro colega, o elemento btnAdicionarTarefa não foi encontrado. Favor rever.")
-    }
-
-    if (!btnCancelar) {
-        throw Error("Caro colega, o elemento btncancelar não foi encontrado. Favor rever.")
-    }
-
-    if (!btnDeletar) {
-        throw Error("Caro colega, o elemento btnDeletar não foi encontrado. Favor rever.")
     }
 
     btnAdicionarTarefa.onclick = () => {
         formAdicionarTarefa?.classList.toggle('hidden')
     }
 
-    btnCancelar.onclick = () =>{
-        console.log("cancelar clicado")
-        formAdicionarTarefa!.classList.add("hidden")
-    }
-
-    btnDeletar.onclick = () => {
-        textarea!.value = ''
-    };
-
-    const formLimparEFechar = () =>{
-        formAdicionarTarefa?.reset()
-        //Opcional
-        // formAdicionarTarefa!.classList.add("hidden")
-    }
-
     formAdicionarTarefa!.onsubmit = (evento) => {
         evento.preventDefault()
-        let descricao = textarea!.value
-        estadoInicial = adicionarTarefa(estadoInicial, { 
+        const descricao = textarea!.value
+        estadoInicial = adicionarTarefa(estadoInicial, {
             descricao,
             concluida: false
         })
         atualizarUI()
-        formLimparEFechar()
+    }
+
+    btnCancelar.onclick = () => {
+        formAdicionarTarefa!.classList.add('hidden');
+    }
+
+    btnDeletar.onclick = () => {
+        estadoInicial = deletar(estadoInicial);
+        formAdicionarTarefa!.classList.add('hidden');
+        atualizarUI();
+    }
+
+    btnDeletarConcluidas.onclick = () => {
+        estadoInicial = deletarTodasConcluidas(estadoInicial);
+        atualizarUI();
+    }
+
+    btnDeletarTodas.onclick = () => {
+        estadoInicial = deletarTodas(estadoInicial);
+        atualizarUI();
     }
 
     if (ulTarefas) {
@@ -110,21 +147,17 @@ const atualizarUI = () => {
         li.classList.add('app__section-task-list-item')
         const svgIcon = document.createElement('svg')
         svgIcon.innerHTML = taskIconSvg
-        
+
         const paragraph = document.createElement('p')
         paragraph.classList.add('app__section-task-list-item-description')
         paragraph.textContent = tarefa.descricao
 
-
         const button = document.createElement('button')
         button.classList.add('app_button-edit')
-    
+
         const editIcon = document.createElement('img')
         editIcon.setAttribute('src', '/imagens/edit.png')
-        editIcon.onclick = () =>{
-            formAdicionarTarefa?.classList.toggle('hidden')
-        }
-    
+
         button.appendChild(editIcon)
 
         if (tarefa.concluida) {
@@ -132,32 +165,36 @@ const atualizarUI = () => {
             li.classList.add('app__section-task-list-item-complete')
         }
 
+        if (tarefa == estadoInicial.tarefaSelecionada) {
+            li.classList.add('app__section-task-list-item-active')
+        }
+
         li.appendChild(svgIcon)
         li.appendChild(paragraph)
         li.appendChild(button)
 
-li.addEventListener('click', () => {
-    console.log('A tarefa foi clicada', tarefa);
-    formAdicionarTarefa?.classList.remove("hidden");
-    let btnAtualizar = document.querySelector<HTMLButtonElement>(".app__form-footer__button--confirm") 
+        li.addEventListener('click', () => {
+            console.log('A tarefa foi clicada', tarefa)
+            estadoInicial = selecionarTarefa(estadoInicial, tarefa)
+            atualizarUI()
+        })
 
-    const indiceTarefaSelecionada = estadoInicial.tarefas.indexOf(tarefa);
-    const valorTarefaSelecionada = estadoInicial.tarefas[indiceTarefaSelecionada].descricao;
-
-    textarea!.value = valorTarefaSelecionada.toString();
-
-    btnAtualizar!.onclick = () => {
-        // Atualizar o estado com a tarefa selecionada
-        estadoInicial.tarefas[indiceTarefaSelecionada].descricao = textarea!.value;
-        atualizarUI();
-        formAdicionarTarefa?.classList.add("hidden"); // Para esconder o formulário após a atualização
-    };
-});
-       
-        
+        // Adicionar evento de clique para editar uma tarefa
+        editIcon.onclick = (evento) => {
+            evento.stopPropagation();
+            estadoInicial = editarTarefa(estadoInicial, tarefa);
+            atualizarUI();
+        }
 
         ulTarefas?.appendChild(li)
     })
 }
+
+document.addEventListener('TarefaFinalizada', () => {
+    if (estadoInicial.tarefaSelecionada) {
+        estadoInicial.tarefaSelecionada.concluida = true
+        atualizarUI()
+    }
+})
 
 atualizarUI()
